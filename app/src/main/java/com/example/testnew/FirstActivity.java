ca591bb668ee39753a324bda6fd64518b27df562
Apu-Mirza <mirzaa012323@gmail.com>
2024-05-13 16:10:26 +0600

    Test purpose

---
+++ app/src/main/java/com/example/testnew/FirstActivity.java
@@ -0,0 +1,28 @@
+package com.example.testnew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("First Activity");
+
+        setContentView(R.layout.activity_main);
+
+        Button btnGoToSecondActivity = findViewById(R.id.btnGoToSecondActivity);
+        btnGoToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
